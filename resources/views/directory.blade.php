595b363de908342d2d677717865020ba45f6124d
DCartist <pbannerman13@gmail.com>
2021-01-07 23:53:35 -0500

    IN-24 updating look of directory

--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="flex justify-center content-end" id="aboutBackgroundHeader" style="background-image: url({{asset('/images/hands.jpg')}});" >
-
 <h1 class="bg-black min-w-full self-end h-11 text-3xl pl-5 text-white">Directory</h1>
-
 
 </div>
 @if ($directories->count())
+
+
 <div class="grid grid-cols-4 gap-2">
             @foreach($directories as $directory)
             <div class="m-3">
