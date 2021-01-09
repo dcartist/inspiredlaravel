dd7b2cbaa8f27eb7e110ca311acd8e75e5037b56
DCartist <pbannerman13@gmail.com>
2021-01-09 17:10:46 -0500

    IN-17 adding infor to imposter syndrome

---
+++ resources/views/imposter.blade.php
@@ -0,0 +1,14 @@
+@extends('base')
+@section('content')
+<div>
+<h1>Imposter Syndrome</h1>
+<div>
+<p>
+Impostor syndrome (also known as impostor phenomenon, impostorism, fraud syndrome or the impostor experience) is a psychological pattern in which an individual doubts their skills, talents or accomplishments and has a persistent internalized fear of being exposed as a "fraud".
+</p>
+</div>
+
+
+
+</div>
+@endsection
