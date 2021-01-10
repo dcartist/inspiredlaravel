873cb1b79da48693943f91cab34c16e16bdb7773
DCartist <pbannerman13@gmail.com>
2021-01-09 19:13:36 -0500

    IN-15 Adding to about section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,10 +8,13 @@
 <h1 class="text-8xl text-center font-extralight pt-4">
     Don't suffer in silence!
 </h1>
-<section class="p-10 ml-5 mr-5">
-
-Create by Paula Bannerman (www.dcartist.studio) as a  prototype for a community for developers, designers, data scientists, and beyond. This is meant to be able to help those in the tech world to find help and support via groups, mantras, and being able to find support sources.
-
+<section class="p-10 ml-5 mr-5 grid grid-cols-4">
+<div>
+<img src="{{asset('/images/paulabannerman.jpg')}}" class="w-72">
+</div>
+<div class="col-span-3 flex items-center">
+Create by Paula Bannerman <a href=""><span>www.dcartist.studio</span></a> as a  prototype for a community for developers, designers, data scientists, and beyond. This is meant to be able to help those in the tech world to find help and support via groups, mantras, and being able to find support sources.
+</div>
 
 </section>
 
