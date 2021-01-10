34008d95bf08e5ee5bcd2b8de7e196fe110636de
DCartist <pbannerman13@gmail.com>
2021-01-10 02:45:40 -0500

    updating App

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //moves to https
         if(config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
